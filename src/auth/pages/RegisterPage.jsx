8769ae8320bdef8077cbc2879d4740252f00122e
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2023-01-15 22:59:13 -0300

    add navbar

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,6 +1,58 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+
+
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <AuthLayout title="Crear cuenta">
+      <form>
+        <Grid container>
+        <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre completo"
+              type="text"
+              placeholder="Macarena Muñoz"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@gmail.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+            
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}> ¿Ya tienes cuenta? </Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/register">
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,16 @@
-import { Typography } from "@mui/material"
+import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1"> Journal Page </Typography>
-      
-    </>
-    
-  )
-}
-
+    <JournalLayout>
+      <Typography>
+        {" "}
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste,
+        blanditiis laborum ducimus doloremque nisi aperiam praesentium possimus
+        illum? Quis molestiae eaque corrupti iusto distinctio cum esse
+        recusandae quidem nam facilis?
+      </Typography>
+    </JournalLayout>
+  );
+};
